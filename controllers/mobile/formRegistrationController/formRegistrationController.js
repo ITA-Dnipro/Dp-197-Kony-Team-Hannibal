296e7b188f64ee93f6044ebf0e8bd9906bcbab79
Oleg <olegdemchenko1993@gmail.com>
2021-02-07 14:17:24 +0200

    change registration form visual

--- controllers/mobile/formRegistrationController/formRegistrationController.js
+++ controllers/mobile/formRegistrationController/formRegistrationController.js
@@ -1,3 +1,7 @@
+function getUniqId() {
+  return new Date().getTime();
+}
+
 function validateUserData(userData) {
   var err;
   switch (true) {
@@ -27,24 +31,39 @@ function validateUserData(userData) {
   return err;
 }
 
-define({ 
- initForm: function() {
-   this.view.btnCreate.onClick = this.createNewUser.bind(this);
- },
+define(['authenticationService'], function(authService) { 
+  return {
+   initForm: function() {
+     this.view.btnSuccess.onClick = this.createNewUser.bind(this);
+     this.view.btnCancel.onClick = function() {
+       navigateToForm('formResult');
+     };
+   },
 
- createNewUser: function createNewUser() {
-   var userData = {
-     fullName: this.view.inputFullName.text || '',
-     email: this.view.inputEmail.text || '',
-     login: this.view.inputLogin.text || '',
-     password: this.view.inputPassword.text || '',
-     passwordConfirm: this.view.inputConfirmPassword.text || '',
-   };
-   var err = validateUserData(userData);
-   if (err) {
+   showErr: function(err) {
      alert(err);
-     return;
+   },
+    
+   showSuccess: function(user) {
+     alert(user);
+   },
+
+   createNewUser: function createNewUser() {
+     var userData = {
+       fullName: this.view.inputFullName.text || '',
+       email: this.view.inputEmail.text || '',
+       login: this.view.inputLogin.text || '',
+       password: this.view.inputPassword.text || '',
+       passwordConfirm: this.view.inputConfirmPassword.text || '',
+     };
+     var err = validateUserData(userData);
+     if (err) {
+       this.showErr(err);
+     } else {
+       delete userData.passwordConfirm;
+       userData.id = getUniqId();
+       authService.registerUser(userData, this.showSuccess.bind(this), this.showErr.bind(this));
+     }
    }
- }
-  
- });
+ };
+});
