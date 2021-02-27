777080df7bb33c72d06ebdd5e1095e9803dfaefd
Oleg <olegdemchenko1993@gmail.com>
2021-02-27 11:49:37 +0200

    replace startRes from appStorage to ResService

--- modules/AppStorage.js
+++ modules/AppStorage.js
@@ -1,15 +1,6 @@
 var appStorage = {
   newsToShow: {},
   userProfile: {},
-  startResources: [
-    { name: 'BBC', url: 'www.bbc.com', logo: 'bbc.jpg' },
-    { name: 'Wall Street Journal', url: 'www.wsj.com', logo: 'wall_street_journal_.jpg' },
-    { name: 'Reuters', url: 'www.reuters.com', logo: 'reuters.png' },
-    { name: 'Al Jazeera', url: 'www.aljazeera.com', logo: 'al_jazeera.jpg' },
-    { name: 'Mirror', url: 'www.mirror.co.uk', logo: 'mirror.png' },
-    { name: 'CBS News', url: 'www.cbsnews.com', logo: 'cbs.jpg' },
-    { name: 'CNBC', url: 'www.cnbc.com', logo: 'cnbc.jpg' },
-  ],
   userResources: [],
   feeds: [],
   news: [],
