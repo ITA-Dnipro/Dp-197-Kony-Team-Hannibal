07af918bccb1f56c4e41b2be3f9c0d9cbc56c66c
Oleg <olegdemchenko1993@gmail.com>
2021-03-03 12:45:21 +0200

    add checking for empty res in fabric findResources

--- modules/require/fabricResourcesService.js
+++ modules/require/fabricResourcesService.js
@@ -1,22 +1,23 @@
 define(['constants'], function (appConstants) {
   var client = kony.sdk.getCurrentInstance();
   var sqlSvc = client.getIntegrationService('MySQLService');
-  
+
   function findResources(url, successCb, errorCb) {
     function extractResources(data) {
-       var resources = data.responseList.reduce(function(acc, feed) {
-           var isResourceRepeated = acc.some(function(addedResource) {
-             return addedResource.name === feed.site_name && addedResource.url === feed.site_url;
-           });
-         return isResourceRepeated ? acc : acc.concat(new NewsResourceModel(feed.site_name, feed.site_url, feed.favicon));
-       }, []);
-       successCb(resources);
-     }
-   
+      var responseResources = data.responseList || [];
+      var uniqResources = responseResources.reduce(function(acc, feed) {
+        var isResourceRepeated = acc.some(function(addedResource) {
+          return addedResource.name === feed.site_name && addedResource.url === feed.site_url;
+        });
+        return isResourceRepeated ? acc : acc.concat(new NewsResourceModel(feed.site_name, feed.site_url, feed.favicon));
+      }, []);
+      successCb(uniqResources);
+    }
+
     var integrationSvc = client.getIntegrationService('RSSFeedService');
     integrationSvc.invokeOperation('getRSSResources', null, { resDomain: url }, extractResources, errorCb);
   } 
-  
+
   function getUserResources(userId, successCb, errorCb) {
     sqlSvc.invokeOperation('getUserResources', null, { userId: userId }, function(resp) {
       var renamedRecords = resp.records.map(function(rec) {
@@ -30,7 +31,7 @@ define(['constants'], function (appConstants) {
       successCb(renamedRecords);
     }, errorCb);
   }
-  
+
   function addResource(userId, newResource, successCb, errorCb) {
     sqlSvc.invokeOperation('addResource', null, {
       curr_user_id: userId,
@@ -39,23 +40,23 @@ define(['constants'], function (appConstants) {
       resource_logo: newResource.logo
     }, successCb, errorCb);
   }
-  
+
   function addResources(userId, newResources, successCb, errorCb) {
     var callback = newResources.length === 1 ? successCb : function() {
       addResources(userId, newResources.slice(1), successCb, errorCb);
     };
-    
+
     addResource(userId, newResources[0], callback, errorCb);
   }
-  
+
   function addStartResources(userId, successCb, errorCb) {
     sqlSvc.invokeOperation('addStartResources', null, { user_id: userId }, successCb, errorCb);
   }
-  
+
   function deleteResource(userId, resourceId, successCb, errorCb) {
     sqlSvc.invokeOperation('deleteUserResource', null, { id: resourceId }, successCb, errorCb);
   }
-  
+
   return {
     findResources: findResources,
     addResources: addResources,
